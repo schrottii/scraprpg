c805d7fdfbc1c37003a6adc80ac638be43cb2971
J S <oh.seyy@yahoo.de>
2021-11-14 15:03:49 +0100

    loadImage function

--- images.js
+++ images.js
@@ -1,3 +1,12 @@
+function loadImage(path, onload) {
+    let img = new Image();
+    img.src = path;
+    if (onload) {
+        img.onload = onload;
+    }
+    return img;
+}
+
 var images =
 {
     gear: loadImage("Images/gear.png"),
