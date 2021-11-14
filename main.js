00ba2457120b1aa8b9bb259a218a89b0e996cd69
J S <oh.seyy@yahoo.de>
2021-11-14 14:37:54 +0100

    add black square?

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ function draw() {
     if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
 
+        ctx.fillStyle = "rgb(255,255,255)";
+        ctx.fillRect(0, 0, 800, 500);
+
         ctx.fillStyle = "rgb(0,25,255)";
         ctx.fillRect(600, 400, 55, 50);
     }
