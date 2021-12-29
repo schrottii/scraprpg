28b241a9bd66765ba1efe366d6b2d808213d4249
DecastarIsNeverACoder <94293937+DecastarIsNeverACoder@users.noreply.github.com>
2021-12-29 09:32:12 +0800

    Bleu Max HP Level Up chart
    Max HP growth rates with each level reached, rates for other characters maybe soon

--- characters.js
+++ characters.js
@@ -3,17 +3,17 @@ characters = {
         name: "Bleu",
         unlocked: true,
         level: 1,
-        EP: 5,
-        maxHP: 50,
-        HP: 50,
+        EP: 3,
+        maxHP: 20,
+        HP: 20,
     },
     corelle : {
         name: "Corelle",
         unlocked: true,
         level: 1,
         EP: 5,
-        maxHP: 40,
-        HP: 40,
+        maxHP: 18,
+        HP: 18,
     }
 }
 
@@ -28,7 +28,25 @@ characters = {
 10th: Johnathan (He's a Walking on Sunshine barrel from Florida)
 11th: Amira (She's a Pink Hex of Doom barrel from Saudi Arabia)
 12th: Lucas (He's a Rhapsody in Green barrel from Brazil)
-*/
+Character level up rates (Max HP):
+Bleu: 20, 24, 30, 35, 41, 46, 50, 53, 59, 63, 66, 70, 75, 81 (lvl 14,)
+      85, 88, 90, 94, 100, 105, 109, 113, 116, 119, 121, 124 (lvl 26,) 
+      127, 129, 132, 136, 138, 144, 150, 155, 159, 163, 166, 169 (lvl 38,)
+      174, 178, 187 (hp rate increased from 2-6 to 9-13,) 197, 208 (lvl 43,)
+      220, 233, 245, 256, 266, 275, 284, 294, 305, 317, 330, 343, 356 (lvl 56,)
+      368, 380, 392, 404 (level 60)
+If Bleu promotes early (at 60,) then 9-13>21-34 hp rate
+Promoted rate (starting at lvl 61): 438, 470, 500, 528, 554, 578, 600, 621, 642 (lvl 69,)
+                                    664, 686, 709, 732, 756, 780, 805, 830, 855 (lvl 78,)
+                                    876, 897, 931, 952, 973, 994, 999 (true cap reached by lvl 85, 
+                                                                      max hp will stay at 999 
+                                                                      no matter how much you level 
+                                                                      up or use stat inc. items)
+Rate without promotion at lvl 61+: 417, 430, 443, 456, 469, 482, 495, 508, 521, 534, 547 (lvl 71,)
+                                   560, 573, 586, 599, 612, 625, 638, 651, 664, 677, 690 (lvl 82,)
+                                   703, 716, 729, 742, 755, 768, 781, 794, 807, 820, 833 (lvl 93,)
+                                   846, 859, 872, 885, 898, 911 (lvl 99, you can increase this cap with stat inc. items but the 999 cap still exists)
+*/       
 
 char1 = "bleu";
 char2 = "corelle";
