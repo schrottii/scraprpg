0a14cb9057890652515fb184511ed81926196b9b
J S <oh.seyy@yahoo.de>
2022-04-10 10:34:50 +0200

    Enemies only on their map now

--- scripts/map_enemies.js
+++ scripts/map_enemies.js
@@ -10,15 +10,16 @@ let mapenemies = {
 
             render(ctx) {
 
-                let tileX = this.position[0];//- game.position[0];
-                let tileY = this.position[1];//- game.position[1];
+                let tileX = this.position[0];
+                let tileY = this.position[1];
 
                 let xAdjust = game.position[0] - width / 2 + 0.5;
-
-                ctx.drawImage(images["gear"],
+                if (game.map == this.map) {
+                    ctx.drawImage(images["gear"],
                     ((zoom * scale) * (tileX + kofs[0] * kofs[2] - xAdjust)) - ((zoom - 1) * scale * (width / 2)),
                     (zoom * scale) * (tileY + kofs[1] * kofs[2] - (game.position[1] - 7.5)) - ((zoom - 1) * scale * 7),
                     zoom * scale, zoom * scale)
+                }
             },
 
             ...args || {},
