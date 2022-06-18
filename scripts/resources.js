aeda913ffa10fd337c872823fb66efb2568710cc
Barduzzi <94293647+Salveeny@users.noreply.github.com>
2022-06-19 00:59:01 +0200

    Update resources.js

--- scripts/resources.js
+++ scripts/resources.js
@@ -99,6 +99,7 @@ let images = {
     "tiles/uv_house_angle_right_up": "data/images/tiles/tile_uv_house_angle_right_up.png",
     "tiles/uv_house_angle_right_down": "data/images/tiles/tile_uv_house_angle_right_down.png",
     "tiles/uv_house_up_window": "data/images/tiles/tile_uv_house_up_window.png",
+    "tiles/teleport": "data/images/tiles/tile_teleport.png",
 };
 
 var scenes = {
