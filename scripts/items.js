9431f2e90fae5a5210f98a95f2f0d0cac3daee19
fn78 <oh.seyy@yahoo.de>
2022-11-01 11:59:53 +0100

    cheaper pants

--- scripts/items.js
+++ scripts/items.js
@@ -145,7 +145,7 @@ let items = {
             ...items.default(),
             source: "scroll",
             name: "Chicago Pants",
-            shopcost: 999999,
+            shopcost: 100,
             type: "clothes",
             max: 1,
             type: "body",
