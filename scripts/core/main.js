242fe45f459abcc0b0558ad87986594ac8f30f0a
fn78 <oh.seyy@yahoo.de>
2026-05-02 17:33:08 +0200

    seperated nodemain.js file for electron (much much smaller scale than in lunaudia)

--- scripts/core/main.js
+++ scripts/core/main.js
@@ -28,58 +28,6 @@ let width = 1920 / scale;// window.innerWidth / scale;
 
 const GAMEVERSION = "v1.1 (INDEV)";
 
-function isElectron() {
-    // Renderer process
-    if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
-        return true;
-    }
-
-    // Main process
-    if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
-        return true;
-    }
-
-    // Detect the user agent when the `nodeIntegration` option is set to true
-    if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
-        return true;
-    }
-
-    return false;
-}
-
-// electron
-if (isElectron()) {
-    const { app, BrowserWindow } = require('electron');
-
-    const createWindow = () => {
-        const win = new BrowserWindow({
-            width: 1920,
-            height: 1080,
-            icon: "styles/favicon/ico.ico",
-        })
-
-        win.setMenuBarVisibility(false);
-        win.loadFile('index.html');
-    }
-
-    app.whenReady().then(() => {
-        createWindow();
-
-        app.on('activate', () => {
-            if (BrowserWindow.getAllWindows().length === 0) {
-                createWindow();
-            }
-        });
-    });
-    /*
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit();
-        }
-    });
-    */
-}
-
 function addNotification(name) {
     if (!notifications.includes(name)) notifications.push(name);
 }
